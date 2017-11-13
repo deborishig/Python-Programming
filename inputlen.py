7d7bb2b832b96d3546b6b2184c5551d4a7fe1954
deborishig <33003016+deborishig@users.noreply.github.com>
2017-11-13 22:26:00 +0530

    Iteration 1
    Final file for the testing of the input() and len() function in Python.

---
+++ inputlen.py
@@ -0,0 +1,6 @@
+# This is a simple program to gauge the use of the input() and len () functions which are built into Python.
+
+
+a= input("What's your name?")
+
+print('There are',len(a),'characters in your name',a)
